15fd51c1f8a6b3f58abfdaad743ce85f42f7a0ab
Валерий <kaktuz34@gmail.com>
2019-02-20 13:33:21 +0200

    upd

--- src/js/components/_fp.js
+++ src/js/components/_fp.js
@@ -3,9 +3,6 @@ import { ANIMATE } from '../constants';
 import IScroll from "iscroll";
 window.IScroll = IScroll;
 var fullpage = require("../lib/jquery.fullpage.min.js");
-// import { init, fpAnimation, fpReset } from './_animation-fullpage';
-
-// var fp = document.getElementById('fullpage');
 
 $(document).ready(function() {
   var slides = $(".js-section");
@@ -22,10 +19,6 @@ $(document).ready(function() {
     fixedElements: ".js-header",
     menu: "#myMenu",
     css3: true,
-    // navigation: true,
-    // fitToSection: false,
-    // paddingTop: 135,
-
     onLeave: function(origin, destination, direction) {
       var leavingSection = this;
       console.log(origin, destination, direction);
@@ -45,67 +38,6 @@ $(document).ready(function() {
       buttons.each((index, el) => $(el).addClass(ANIMATE));
     }
   });
-  // afterRender: () => {
-  //   $.fn.fullpage.setAllowScrolling(false);
-  //   $.fn.fullpage.setKeyboardScrolling(false);
-
-  //   if ($(window).width() < 767) {
-  //     fpAnimation($('.section:nth-child(2)'));
-  //   }
-  // },
-
-  // onLeave: function(origin, destination, direction) {
-  //   var loadedSection = this;
-
-  //   if ($(window).width() > 767) {
-  //     setTimeout(() => {
-  //       if (direction === 'down') {
-  //         var prevSlide = slides[destination - 2];
-  //         fpReset(prevSlide);
-  //       }
-  //       if (direction === 'up') {
-  //         var nextSlide = slides[destination];
-  //         fpReset(nextSlide);
-  //       }
-  //     }, 500);
-  //   }
-
-  //   /*
-  //   * Changing pagination text and email/Russia in footer
-  //   */
-  //   if (destination === 1) {
-  //     pg.text('Scroll');
-  //     $('.js-footer-hidden').addClass('is-hidden');
-  //     $('.js-footer-visible').removeClass('is-hidden');
-  //   } else if (destination === $('.section').length) {
-  //     $('.js-footer-hidden').removeClass('is-hidden');
-  //     $('.js-footer-visible').addClass('is-hidden');
-  //     pg.html('<button class="up" id="up">Up↑</button>');
-  //   } else {
-  //     $('.js-footer-hidden').addClass('is-hidden');
-  //     $('.js-footer-visible').removeClass('is-hidden');
-  //     if (destination < 9) {
-  //       pg.text('0' + (destination - 1) + '/');
-  //     } else {
-  //       pg.text((destination - 1) + '/');
-  //     }
-  //   }
-  // },
-
-  // afterLoad: function(index, destination, direction) {
-  //   $.fn.fullpage.setAllowScrolling(false);
-  //   $.fn.fullpage.setKeyboardScrolling(false);
-
-  //   var up = document.getElementById('up');
-  //   if (up) {
-  //     up.addEventListener('click', (e) => {
-  //       $.fn.fullpage.moveTo(1);
-  //     })
-  //   }
-
-  //   var loadedSection = this;
-  //   fpAnimation(loadedSection);
-
   const wrap = $('.js-sections-wrapper');
   const sectionDev = $('.js-section-development');
   const sectionInv = $('.js-section-investment');
@@ -122,5 +54,3 @@ $(document).ready(function() {
   });
 
 });
-//   }
-// });
